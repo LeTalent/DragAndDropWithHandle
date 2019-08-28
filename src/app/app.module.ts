1d875e98c5e9e1ec160ee9329f4c9453c5e7d9bb
Maurice <MauriceNgassa.Happi@antrago.de>
2019-08-28 14:55:13 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {DragDropModule} from '@angular/cdk/drag-drop';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
